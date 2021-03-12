e38a39d7c13fee1e1fd4fa7e021def9d535c066c
dawnsoup <atxm@163.com>
2021-03-12 18:17:21 +0800

    0311

--- app/src/main/java/com/example/android/logic/Repository.kt
+++ app/src/main/java/com/example/android/logic/Repository.kt
@@ -1,9 +1,12 @@
 package com.example.android.logic
 
 import androidx.lifecycle.liveData
+import com.example.android.logic.model.Weather
 import com.example.android.logic.network.SunnyWeatherNetwork
 import com.sunnyweather.android.logic.model.Place
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
 import java.lang.Exception
 import java.lang.RuntimeException
 
@@ -20,6 +23,37 @@ object Repository {
             }
         } catch (e: Exception) {
             Result.failure<List<Place>>(e)
+        }
+        emit(result)
+    }
+
+
+    fun refreshWeather(lng: String, lat: String) = liveData(Dispatchers.IO) {
+        val result = try {
+            coroutineScope {
+                val deferredRealtiem = async {
+                    SunnyWeatherNetwork.getRealtimeWeather(lng, lat)
+                }
+                val deferredDaily = async {
+                    SunnyWeatherNetwork.getDailyWeather(lng, lat)
+                }
+                val realtimeResponse = deferredRealtiem.await()
+                val dailyResponse = deferredDaily.await()
+                if (realtimeResponse.status == "ok" && dailyResponse.status == "ok") {
+                    val weather = Weather(realtimeResponse.result.realtime, dailyResponse.result.daily)
+                    Result.success(weather)
+                } else {
+                    Result.failure(
+                        RuntimeException(
+                            "realtime response status is ${realtimeResponse.status}" +
+                                    "daily response status is ${dailyResponse.status}"
+                        )
+                    )
+                }
+            }
+
+        } catch (e: Exception) {
+            Result.failure<Weather>(e)
         }
         emit(result)
     }
